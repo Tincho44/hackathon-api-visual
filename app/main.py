77de1133f9c7579e227e0483e15e5f00f239262d
nicolas <nicolas.rodriguez@1nce.com>
2025-06-07 17:43:16 -0300

    Working version of YOLO pre trained with live detection.

--- app/main.py
+++ app/main.py
@@ -55,20 +55,6 @@ def start_camera_process():
     camera_process = multiprocessing.Process(target=main, daemon=True)
     camera_process.start()
 
-ASSETS_DIR = "assets"
-DEFAULT_IMAGE_PATH = os.path.join(ASSETS_DIR, "image2.png")
-
-@app.get("/yolo")
-async def yolo_detect():
-    if not os.path.exists(DEFAULT_IMAGE_PATH):
-        return {"error": "Imagen no encontrada."}
-
-    with open(DEFAULT_IMAGE_PATH, "rb") as f:
-        contents = f.read()
-
-    detections = detect_objects(contents)
-    return {"filename": os.path.basename(DEFAULT_IMAGE_PATH), "detections": detections}
-
 @app.get("/")
 async def read_root():
     return {"message": "¡Hola desde FastAPI!"}
